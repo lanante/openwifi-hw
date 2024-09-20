cf1db009f978f27acf748dee796c46058ef1acdd
leo <leonardolanante@gmail.com>
2024-09-20 07:57:36 -0400

    Updated repos

--- get_ip_openofdm_rx.sh
+++ get_ip_openofdm_rx.sh
@@ -7,7 +7,7 @@ cd ip/
 git submodule init openofdm_rx
 git submodule update openofdm_rx
 cd openofdm_rx
-git checkout dot11zynq
-git pull origin dot11zynq
+git checkout dot11zynq_experimental
+git pull origin dot11zynq_experimental
 
 cd $home_dir
